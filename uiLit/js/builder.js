c473c11c33dc0452e4345853b9d30752c0b23373
Jdhayford <jdhayford@gmail.com>
2016-07-26 21:48:55 -0700

    Refined scroll transitions and cleaned code base

--- uiLit/js/builder.js
+++ uiLit/js/builder.js
@@ -5,38 +5,46 @@ var state = 'din';
 
 var height = $('#header').css('height');
 
+// On picking a nav block, use the info to toggle the Navigation state
+// between fitness and dining
 $('.navBlock').on('click', function(){
       state = $(this).attr('id');
       updateState(state);
   });
 
 var activeLocation = 0;
+  // On picking a location block, use the location block info to filter eateries
   $('.locBlock').on('click', function(){
       activeLocation = $(this).attr('id')*1;
       filterEateries();
   });
 
-// Hide header when scrolling down
+// Hide header Logo when scrolling down
 $('.din-wrapper').on({'mousewheel': function(event) {
     if( ($(window).scrollTop()==0 && $('#header').css('height') == height) && (event.originalEvent.detail > 0 || event.originalEvent.wheelDelta < 0 )) { //alternative options for wheelData: wheelDeltaX & wheelDeltaY
       //scroll down
       $('#header').velocity({
           height:'0',
+          margin: '0',
           opacity:'0',
           overflow:'hidden',
-          transform:'translateY(80px)'
-        },300);
+          transform:'translateY(100px)'
+        },{duration:150,easing:"linear"});
       return false;
       }
     }    
 })
 
-// Show header when reaching the top
+// Show header Logo when reaching the top
 $('.din-wrapper').on('scroll', function(e) {
   if ($('.din-wrapper').scrollTop() == 0) {
-    $('#header').css('overflow','visible');
-    $('#header').css('height',height);
-    $('#header').css('opacity','1');
+    $('#header').velocity({
+          height:height,
+          'margin-top': '5px',
+          opacity:'1',
+          overflow:'visible',
+          transform:'none'
+        },{duration:150,easing:"linear"});
   }
 })
 
@@ -45,6 +53,7 @@ $('.din-wrapper').on('scroll', function(e) {
 $(document).swipe({
     left: function () {
       console.log('left');
+      // If on a dining location, transition to the next location
       if (activeLocation == 3) {
         return;
       } else if (state == 'din') {
@@ -53,33 +62,32 @@ $(document).swipe({
       }
     },
     right: function () {
+      // If on a dining location, transition to prior location
       if (state == 'din') {
         if (activeLocation == 0) {
           return;
         } else {
           activeLocation = Math.max(0,activeLocation-1);
           filterEateries();
-      }
-      } else {
-        state = 'din';
-        updateState(state);
+        }
       }
     },
     up: function () {
       // Hide header logo when scrolling downward from top
         if( $(window).scrollTop()==0 && $('#header').css('height') == height) {
-          
           $('#header').velocity({
-          height:'0',
-          opacity:'0',
-          overflow:'hidden'
-        },50);
+            height:'0',
+            opacity:'0',
+            margin:'0',
+            overflow:'hidden'
+        },{duration:150,easing:"linear"});
         }
     },
     down: function () {
         console.log("down");
     }
 });
+
 
 function filterEateries() {
   $('.locBlock').removeClass('active');
@@ -102,6 +110,7 @@ function filterEateries() {
   
 }
 
+// Based on the state, toggle between Fitness and Dining
 function updateState(state) {
   $('.navBlock').removeClass('active');
   $("#"+state+"").addClass('active');
@@ -116,6 +125,7 @@ function updateState(state) {
       }
 }
 
+// Set up data visuals for fitness rows
 function fitViz() {
 
   var bars = Array.prototype.slice.call(document.getElementsByClassName("flex-bar"));
@@ -142,6 +152,7 @@ function fitViz() {
   });
 }
   
+// Set up data visuals for dining rows
 function dinViz() {
     var max = 150.72259521484375;
     var shade;
